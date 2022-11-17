e12ec94fa3c5d985bd2521d253832b0b6f22586f
Manor Zvi <manor.zvi@lambda.csf.technion.ac.il>
2022-11-17 13:51:51 +0200

    HW1 v0

--- HW1/matmul_functions.py
+++ HW1/matmul_functions.py
@@ -1,28 +1,74 @@
+import math
 import numpy as np
 from numba import njit, cuda
 import timeit
 
 
 def matmul_transpose_trivial(X):
+    M = X.shape[0]
+    K = X.shape[1]
+    N = X.shape[0]
+    Y = np.zeros((M, M))
+    for m in range(M):
+        for n in range(m+1):
+            for k in range(K):
+                Y[m, n] += X[m, k] * X[n, k]
+            Y[n, m] = Y[m, n]
+    return  Y
     raise NotImplementedError("To be implemented")
 
 
 @njit
 def matmul_transpose_numba(X):
+    # return matmul_transpose_trivial(X)
+    M = X.shape[0]
+    K = X.shape[1]
+    N = X.shape[0]
+    Y = np.zeros((M, M))
+    for m in range(M):
+        for n in range(m+1):
+            for k in range(K):
+                Y[m, n] += X[m, k] * X[n, k]
+            Y[n, m] = Y[m, n]
+    return  Y
     raise NotImplementedError("To be implemented")
 
 
 def matmul_transpose_gpu(X):
+    threads_per_block = (1024)
+    blocks_per_grid = (1)
+
+    X_d = cuda.to_device(X)
+    C = np.zeros((X.shape[0], X.shape[0]))
+    C_d = cuda.to_device(C)
+    matmul_kernel[blocks_per_grid, threads_per_block](X_d, C_d)
+    C = C_d.copy_to_host()
+    return C
     raise NotImplementedError("To be implemented")
 
 @cuda.jit
 def matmul_kernel(A, C):
+    n_elements = int((A.shape[0] * (1 + A.shape[0])) / 2)
+    tid = cuda.threadIdx.x
+    bdim = cuda.blockDim.x
+    thread_section_size = int(math.floor(n_elements // bdim)) if tid < bdim - 1 else n_elements - int((bdim-1) * math.floor(n_elements // bdim))
+    last_thread_section_size = n_elements - int((bdim-1) * math.floor(n_elements // bdim))
+    thread_section_start = int(tid * thread_section_size) if tid < bdim -1 else int(tid * math.floor(n_elements // bdim))
+    thread_section_end = int(thread_section_start + (thread_section_size if (tid < bdim-1) else last_thread_section_size))
+    thread_section_size = thread_section_size if (tid < bdim-1) else last_thread_section_size
+    
+    for i in range(thread_section_start, thread_section_end):
+        x = int((math.sqrt(8 * i + 1) - 1) / 2)
+        y = int(i - x * (x + 1) / 2)
+        for k in range(A.shape[1]):
+            C[x, y] += A[x, k] * A[y, k]
+        C[y, x] = C[x, y]
     raise NotImplementedError("To be implemented")
 
 #this is the comparison function - keep it as it is, don't change X or Y.
 def matmul_comparison():
     X = np.random.randn(784, 128)
-	Xt = X.copy().transpose()
+    Xt = X.copy().transpose()
     def timer(f, functionParameters):
         return min(timeit.Timer(lambda: f(X) if functionParameters == 1 else f(X,Xt)).repeat(3, 100))
 
@@ -34,3 +80,13 @@ def matmul_comparison():
 
 if __name__ == '__main__':
     matmul_comparison()
+
+    X = np.reshape(np.arange(784*128), (784, 128))
+    # X = np.random.randn(784, 128)
+    Xt = X.copy().transpose()
+    
+    XXt = matmul_transpose_numba(X)
+    print(not np.any(np.matmul(X, Xt) - XXt))
+
+    XXt = matmul_transpose_gpu(X)
+    print(not np.any(np.matmul(X, Xt) - XXt))
--- HW1/max_functions.py
+++ HW1/max_functions.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from numba import cuda, njit, prange, float32
 import timeit
@@ -5,33 +6,68 @@ import timeit
 
 def max_cpu(A, B):
     """
-     Returns
-     -------
-     np.array
-         element-wise maximum between A and B
-     """
+    Returns
+    -------
+    np.array
+        element-wise maximum between A and B
+    """
+    # return np.maximum(A, B)
+    C = np.empty_like(A)
+    for x in range(A.shape[0]):
+        for y in range(A.shape[1]):
+            a = A[x, y]
+            b = B[x, y]
+            C[x, y] = max(a, b)
+    return C
     pass
 
-
-@njit(parallel=True)
+# NumbaPerformanceWarning: The keyword argument 'parallel=True' was specified but no transformation for parallel execution was possible..
+# @njit(parallel=True)
+@njit()
 def max_numba(A, B):
     """
-     Returns
-     -------
-     np.array
-         element-wise maximum between A and B
-     """
+    Returns
+    -------
+    np.array
+        element-wise maximum between A and B
+    """
+    # return np.maximum(A, B)
+    C = np.empty_like(A)
+    for x in range(A.shape[0]):
+        for y in range(A.shape[1]):
+            C[x, y] = max(A[x, y], B[x, y])
+    return C
     pass
 
+
+@cuda.jit
+def max_gpu_single_thread(A, B, C):
+    x, y = cuda.grid(2)
+    if x < A.shape[0] and y < A.shape[1]:
+        C[x, y] = max(A[x, y], B[x,y])
 
 def max_gpu(A, B):
     """
-     Returns
-     -------
-     np.array
-         element-wise maximum between A and B
-     """
-    pass
+    Returns
+    -------
+    np.array
+        element-wise maximum between A and B
+    """
+
+    threads_per_block = (1, A.shape[1])
+    blocks_per_grid = (A.shape[0], 1)
+
+
+    A_d = cuda.to_device(A)
+    B_d = cuda.to_device(B)
+    C = np.empty_like(A)
+    C_d = cuda.to_device(C)
+    
+    max_gpu_single_thread[blocks_per_grid, threads_per_block](A_d, B_d, C_d)
+
+    C = C_d.copy_to_host()
+
+    return C
 
 
 @cuda.jit
@@ -41,8 +77,9 @@ def max_kernel(A, B, C):
 
 # this is the comparison function - keep it as it is.
 def max_comparison():
-    A = np.random.randint(0, 256, (1000, 1000))
-    B = np.random.randint(0, 256, (1000, 1000))
+    N = 1000
+    A = np.random.randint(0, 256, (N, N))
+    B = np.random.randint(0, 256, (N, N))
 
     def timer(f):
         return min(timeit.Timer(lambda: f(A, B)).repeat(3, 20))
@@ -53,4 +90,20 @@ def max_comparison():
 
 
 if __name__ == '__main__':
+    os.system('lscpu')
+    os.system('nvidia-smi')
+    os.system('nvidia-smi --query-gpu=name --format=csv')
     max_comparison()
+
+    # N = 1000
+    # A = np.random.randint(0, 256, (N, N))
+    # B = np.random.randint(0, 256, (N, N))
+
+    # print('max_cpu')
+    # print(not np.any(np.maximum(A, B) - max_cpu(A, B)))
+
+    # print('max_numba')
+    # print(not np.any(np.maximum(A, B) - max_numba(A, B)))
+
+    # print('max_gpu')
+    # print(not np.any(np.maximum(A, B) - max_gpu(A, B)))
