a15db02c71cbcdb86c11b98186762568a61388b3
Manor Zvi <manor.zvi@lambda.csf.technion.ac.il>
2022-12-18 09:28:51 +0200

    HW#3 Q2

--- HW2/ip_network.py
+++ HW2/ip_network.py
@@ -1,6 +1,26 @@
+import sys
 import math
+import time
+import matplotlib.pyplot as plt
+
 from network import *
 from preprocessor import *
+from my_queue import MyQueue
+
+def plot_minibatch(data, labels, indices, batch_size, k):
+    
+    def one_hot_to_number(x):
+        return np.where(x == 1)[0].item()
+    
+    rows = math.ceil(math.sqrt(batch_size))
+    cols = math.ceil(math.sqrt(batch_size))
+    fig, axes = plt.subplots(rows, cols)
+    plt.tight_layout()
+    for i,ax in enumerate(axes.flat):
+        ax.imshow(data[indices[i]].reshape((28,28)))
+        ax.set_title(one_hot_to_number(labels[indices[i]]))
+    plt.show()
+    fig.savefig(f'original_minibatch{k}')
 
 class IPNeuralNetwork(NeuralNetwork):
     
@@ -10,13 +30,13 @@ class IPNeuralNetwork(NeuralNetwork):
         '''
         self.jobs = JoinableQueue()
         self.result = Queue()
+        # self.result = MyQueue()
 
         self.num_workers = multiprocessing.cpu_count() * 2
         workers = [ Worker(self.jobs, self.result, training_data, self.mini_batch_size) for i in range(self.num_workers) ]
 
         for w in workers:
             w.start()
-        
 		
         # Call the parent's fit. Notice how create_batches is called inside super.fit().
         super().fit(training_data, validation_data)
@@ -36,37 +56,10 @@ class IPNeuralNetwork(NeuralNetwork):
         '''
         for k in range(self.number_of_batches):
             indices = random.sample(range(0, data.shape[0]), batch_size)
-
-            # import matplotlib.pyplot as plt
-            # def one_hot_to_number(x):
-            #     return np.where(x == 1)[0].item()
-            # rows = math.ceil(math.sqrt(batch_size))
-            # cols = math.ceil(math.sqrt(batch_size))
-            # fig, axes = plt.subplots(rows, cols)
-            # plt.tight_layout()
-            # for i,ax in enumerate(axes.flat):
-            #     ax.imshow(data[indices[i]].reshape((28,28)))
-            #     ax.set_title(one_hot_to_number(labels[indices[i]]))
-            # plt.show()
-            # fig.savefig(f'original_minibatch{k}')
-
+            
             job = {'batch ID': k, 'indices ID': indices}
             self.jobs.put(job)
-
-            # augmented_minibatch = self.result.get()
-            # rows = math.ceil(math.sqrt(batch_size))
-            # cols = math.ceil(math.sqrt(batch_size))
-            # fig, axes = plt.subplots(rows, cols)
-            # plt.tight_layout()
-            # for i,ax in enumerate(axes.flat):
-            #     ax.imshow(augmented_minibatch[0][i].reshape((28,28)))
-            #     ax.set_title(one_hot_to_number(augmented_minibatch[1][i]))
-            # plt.show()
-            # fig.savefig(f'augmented_minibatch{k}')
             
-        # for _ in range(self.num_workers):
-        #     self.jobs.put(None)
-
         batches = []
         for k in range(self.number_of_batches):
             batches.append(self.result.get())
--- HW2/my_queue.py
+++ HW2/my_queue.py
@@ -1,10 +1,19 @@
 
+import os
+from multiprocessing import Pipe, Lock, Value
+
+# counter = 0
+
 class MyQueue(object):
 
     def __init__(self):
         ''' Initialize MyQueue and it's members.
         '''
-        raise NotImplementedError("To be implemented")
+        self.parent_conn, self.child_conn = Pipe()
+        self.lock = Lock()
+        # global counter
+        # self.counter = 0
+        self.counter = Value('i', 0)  # Shared Memory based counter.
 
     def put(self, msg):
         '''Put the given message in queue.
@@ -14,8 +23,16 @@ class MyQueue(object):
         msg : object
             the message to put.
         '''
-        raise NotImplementedError("To be implemented")
-
+        self.lock.acquire()
+        try:    
+            # print(os.getppid(), os.getpid(), id(self.counter), self.counter.value)
+            self.child_conn.send(msg)
+            # self.counter += 1
+            self.counter.value += 1
+        finally:
+            self.lock.release()
+        
+        
     def get(self):
         '''Get the next message from queue (FIFO)
             
@@ -23,7 +40,12 @@ class MyQueue(object):
         ------
         An object
         '''
-        raise NotImplementedError("To be implemented")
+        msg = self.parent_conn.recv()
+        # print(os.getpid(), id(self.counter), self.counter.value)
+        # self.counter -= 1
+        # with self.counter.get_lock():
+        self.counter.value -= 1
+        return msg
     
     def length(self):
         '''Get the number of messages currently in the queue
@@ -32,4 +54,5 @@ class MyQueue(object):
         ------
         An integer
         '''
-        raise NotImplementedError("To be implemented")
+        # return self.counter
+        return self.counter.value
