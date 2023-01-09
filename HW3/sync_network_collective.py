fe85af13c363824ce73f3009bfd345e334959031
Manor Zvi <manor.zvi@lambda.csf.technion.ac.il>
2023-01-09 17:33:01 +0200

    sync training using MPI native Allreduce

--- HW3/sync_network_collective.py
+++ HW3/sync_network_collective.py
@@ -21,15 +21,31 @@ class SynchronicNeuralNetwork(NeuralNetwork):
             labels = training_data[1]
             mini_batches = self.create_batches(data, labels, self.mini_batch_size // size)
 
+            nabla_w = [np.zeros_like(w) for w in self.weights]
+            nabla_b = [np.zeros_like(b) for b in self.biases]
+
             for x, y in mini_batches:
                 # doing props
                 self.forward_prop(x)
                 ma_nabla_b, ma_nabla_w = self.back_prop(y)
 
+                # NOTE: moved outside the loop for better memory allocation efficiency
                 # summing all ma_nabla_b and ma_nabla_w to nabla_w and nabla_b
-                nabla_w = []
-                nabla_b = []
-                # TODO: add your code
+                # nabla_w = [np.zeros_like(w) for w in self.weights]
+                # nabla_b = [np.zeros_like(b) for b in self.biases]
+                
+                for ma_nab_w, ma_nab_b, nab_w, nab_b in zip(ma_nabla_w, ma_nabla_b, nabla_w, nabla_b):
+                    comm.Allreduce(ma_nab_w, nab_w, op=MPI.SUM)
+                    comm.Allreduce(ma_nab_b, nab_b, op=MPI.SUM)
+                
+                # TODO: I would be glad to find a way to pack list of numpy array and send in one piece...
+                # comm.Allreduce(ma_nabla_w, nabla_w, op=MPI.SUM)
+                # comm.Allreduce(ma_nabla_b, nabla_b, op=MPI.SUM)
+
+                # NOTE: although I should have averaged the grads (tutorial 7, slide 13), I noticed that in this simple case, it works also with sum only.
+                # for w, b in zip(nabla_w, nabla_b):
+                #     w = w/size
+                #     b = b/size
 
                 # calculate work
                 self.weights = [w - self.eta * dw for w, dw in zip(self.weights, nabla_w)]
