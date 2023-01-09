4a06854edcdf6ac26727812bc3d271a40caed76c
Manor Zvi <manor.zvi@lambda.csf.technion.ac.il>
2023-01-09 18:00:27 +0200

    add dtype=np.float64 to the dummy layer of network to solve the Allreduce weird problem

--- HW3/sync_network_collective.py
+++ HW3/sync_network_collective.py
@@ -15,6 +15,10 @@ class SynchronicNeuralNetwork(NeuralNetwork):
         rank = comm.Get_rank()
         size = comm.Get_size()
 
+        # replace the 1st dummy layer of weights with the same array by with dtype=np.float64,
+        # due to some weird problem in comm.Allreduce: ValueError: mismatch in send and receive MPI datatypes
+        self.weights[0] = [np.array([0], dtype=np.float64)]
+
         for epoch in range(self.epochs):
 
             data = training_data[0]
@@ -54,3 +58,20 @@ class SynchronicNeuralNetwork(NeuralNetwork):
             self.print_progress(validation_data, epoch)
 
         MPI.Finalize()
+    
+    def back_prop(self, y):
+        nabla_b = zeros_biases(self.sizes)
+        # replace the 1st dummy layer of weights grads with the same array by with dtype=np.float64,
+        # due to some weird problem in comm.Allreduce: ValueError: mismatch in send and receive MPI datatypes
+        # nabla_w = [np.array([0])] + zeros_weights(self.sizes)
+        nabla_w = [np.array([0], dtype=np.float64)] + zeros_weights(self.sizes)
+        error = (self.activations[-1] - y) * sigmoid_prime(self.zs[-1])
+        nabla_b[-1] = np.sum(error, axis=0)
+        nabla_w[-1] = self.matmul(self.activations[-2].T, error)
+
+        for l in range(self.num_layers - 2, 0, -1):
+            error = self.matmul(error, self.weights[l + 1].T) * sigmoid_prime(self.zs[l])
+            nabla_b[l] = np.sum(error, axis=0)
+            nabla_w[l] = self.matmul(self.activations[l - 1].T, error)
+
+        return nabla_b, nabla_w
